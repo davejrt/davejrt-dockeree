a3e5e0c73176862c1bff95588ee6ef8130355ba4
Dave Try <dave.try@puppet.com>
2018-03-21 21:51:10 +1100

    test

--- manifests/package.pp
+++ manifests/package.pp
@@ -10,15 +10,17 @@ class dockeree::package (
 
   exec { 'install_Docker':
    provider => powershell,
-   command => 'Install-Package -Name docker -ProviderName DockerMsftProvider -Force -RequiredVersion',
+   command => 'Install-Package -Name docker -ProviderName DockerMsftProvider -Force',
    unless => 'Get-Package docker',
+   require => Exec['install_DockerMsftProvider'],
    notify => Reboot['after_install'],
   }
 
-  reboot { 'after_install':}
+  # reboot { 'after_install':}
 
   service { 'Docker':
     enable => true,
     ensure => 'running',
+    require => Exec['install_Docker'],
   }
 }
