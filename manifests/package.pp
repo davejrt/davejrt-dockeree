f5915e4f70c1bd8a226cf9851ab7c8dd9e0800c6
Dave Try <dave.try@puppet.com>
2018-03-21 20:02:17 +1100

    testing

--- manifests/package.pp
+++ manifests/package.pp
@@ -5,7 +5,7 @@ class dockeree::package (
   exec { 'install_DockerMsftProvider':
     provider => powershell,
     command => 'Install-Module DockerMsftProvider -Force',
-    unless => 'Get-InstalledModule -Name DockerMsftProvider',
+    unless => 'Get-Module  | findstr DockerMsftProvider',
   }
 
   exec { 'install_Docker':
